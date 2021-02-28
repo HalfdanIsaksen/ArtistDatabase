eadcdb33ad990b33eee4e9f5102e5fc8d3548275
HalfdanIsaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2021-02-28 23:54:22 +0100

    Initializing

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArtistDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
