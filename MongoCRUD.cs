04cbb8e342943e9eead3d2f1c34c2cfb3caebe2c
HalfdanIsaksen <56478241+HalfdanIsaksen@users.noreply.github.com>
2021-03-08 22:53:11 +0100

    Createconnection

--- MongoCRUD.cs
+++ MongoCRUD.cs
@@ -1,9 +1,1 @@
 //CRUD = Create Read Update Delete
-public class MongoCRUD
-{
-    //private
-
-    public MongoCRUD(string database){
-
-    }
-}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace ArtistDatabase
 {
@@ -6,7 +8,23 @@ namespace ArtistDatabase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello world!");
+            MongoCRUD db = new MongoCRUD("Artist"); 
+            Console.ReadLine();
+        }
+    }
+    public class MongoCRUD
+        {
+        private IMongoDatabase db;
+        public MongoCRUD(string database){
+            //create client for connecting to database
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        public void InsertRecord<T>(string table, T record){
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
         }
     }
 }
